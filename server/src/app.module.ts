d42d9fb9b1d846b57a008255909958d6248f54d4
Bahyu Sanciko <41245088+bahyusanciko@users.noreply.github.com>
2021-06-20 15:08:43 +0700

    env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,7 +7,18 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: parseInt(process.env.DATABASE_PORT) || 3306,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true
+      })
+    }),
     UsersModule,
     AuthModule
   ],
